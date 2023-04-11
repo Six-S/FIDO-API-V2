1d19136c07d9e46739b59ed5ddd1eea564a73d59
Six-S <brenlink@gmail.com>
2023-04-10 22:54:27 -0600

    add health check endpoint as I continue to learn about Micronaut and stuff.

--- src/main/java/com/fido/api/Application.java
+++ src/main/java/com/fido/api/Application.java
@@ -1,6 +1,10 @@
 package com.fido.api;
 
+import io.micronaut.http.annotation.Controller;
 import io.micronaut.runtime.Micronaut;
+import io.micronaut.validation.Validated;
+import jakarta.inject.Inject;
+import lombok.RequiredArgsConstructor;
 
 public class Application {
 
