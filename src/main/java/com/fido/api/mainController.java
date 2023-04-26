c2a9c4917c7a9ea8ddc603bbf9c25d80147555bc
Six-S <brenlink@gmail.com>
2023-04-25 21:43:37 -0600

    humble beginnings.

--- src/main/java/com/fido/api/mainController.java
+++ src/main/java/com/fido/api/mainController.java
@@ -1,22 +1,34 @@
 package com.fido.api;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import java.util.HashMap;
 
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
-import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.annotation.*;
+
+
 
 @Controller("/command")
 public class mainController {
     @Get
     @Produces(MediaType.APPLICATION_JSON)
+//    @Consumes(MediaType.APPLICATION_JSON)
     public HashMap<String, String> index() {
         HashMap<String, String> returnObject = new HashMap<>();
         returnObject.put("test1", "value1");
         returnObject.put("test2", "value2");
 
         return returnObject;
+    }
+
+    @Post("/text")
+    public HttpResponse<?> test(HttpRequest<ObjectNode> request)
+    {
+
+        System.out.println(request.getBody().get());
+        return HttpResponse.ok();
     }
 }
