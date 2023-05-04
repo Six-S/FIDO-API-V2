8b9f898ad0efe54dad5aca7ce00d5decf4801c66
Six-S <brenlink@gmail.com>
2023-05-03 21:57:20 -0600

    Add openai-java library, mess around with functionality here, trying to figure out if I should be using a lib like this or write my own interface.

--- src/main/java/com/fido/api/mainController.java
+++ src/main/java/com/fido/api/mainController.java
@@ -1,15 +1,23 @@
 package com.fido.api;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import java.util.HashMap;
-
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionResult;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.service.OpenAiService;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
-import io.micronaut.http.annotation.*;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Produces;
 
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Controller("/command")
 public class mainController {
@@ -23,12 +31,29 @@ public class mainController {
 
         return returnObject;
     }
-
     @Post("/text")
     public HttpResponse<?> test(HttpRequest<ObjectNode> request)
     {
+        OpenAiService service = new OpenAiService(System.getenv("OPENAI_API_KEY"));
+        System.out.println("Streaming chat completion...");
+        final List<ChatMessage> messages = new ArrayList<>();
+        final ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), "Hello! I hope you are doing well.");
+        messages.add(systemMessage);
+        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
+                .builder()
+                .model("gpt-3.5-turbo")
+                .messages(messages)
+                .n(1)
+                .maxTokens(50)
+                .logitBias(new HashMap<>())
+                .build();
 
-        System.out.println(request.getBody().get());
+        System.out.println(service.createChatCompletion(chatCompletionRequest));
+//        service.streamChatCompletion(chatCompletionRequest)
+//                .doOnError(Throwable::printStackTrace)
+//                .blockingForEach(System.out::println);
+
+        System.out.println(request.getBody().get().findValue("test"));
         return HttpResponse.ok();
     }
 }
